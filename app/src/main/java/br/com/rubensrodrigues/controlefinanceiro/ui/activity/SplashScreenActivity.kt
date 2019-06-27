3c5abf1b0b0310a0ed0c392f337eac14b7b05371
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2019-06-27 01:39:36 -0300

    Criado SplashScreen e a logo da Finpoup

---
+++ app/src/main/java/br/com/rubensrodrigues/controlefinanceiro/ui/activity/SplashScreenActivity.kt
@@ -0,0 +1,44 @@
+package br.com.rubensrodrigues.controlefinanceiro.ui.activity
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import br.com.rubensrodrigues.controlefinanceiro.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private var tempo: Long = 500
+
+    private val CHAVE_SPLASH = "splash"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        verificaSePrimeiroAcesso()
+        executeThread()
+    }
+
+    private fun verificaSePrimeiroAcesso() {
+        val preferences = getSharedPreferences("user_preferences", Context.MODE_PRIVATE)
+        if (!preferences.contains(CHAVE_SPLASH)) {
+            tempo = 2000
+
+            val editor = preferences.edit()
+            editor.putString(CHAVE_SPLASH, "primeiro acesso realizado")
+            editor.apply()
+        }
+    }
+
+    private fun executeThread() {
+        Handler().postDelayed(object : Runnable {
+            override fun run() {
+                startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+                finish()
+            }
+        }, tempo)
+    }
+}
