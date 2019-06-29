aa421093dbda2a4490b8307269899d01edf254e7
Rubens Rodrigues <rubensrodriguesv2@gmail.com>
2019-06-29 13:57:10 -0300

    Feito o mini fab para transferencia de saldo

---
+++ app/src/main/java/br/com/rubensrodrigues/controlefinanceiro/ui/activity/TransferenciaActivity.kt
@@ -0,0 +1,13 @@
+package br.com.rubensrodrigues.controlefinanceiro.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.rubensrodrigues.controlefinanceiro.R
+
+class TransferenciaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transferencia)
+    }
+}
